e673b2c7950203403f134ebb95741eaed500d8fb
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-08-28 03:03:21 +0530

    Added image, appbar

--- lib/main.dart
+++ lib/main.dart
@@ -20,11 +20,25 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('NotShark'),
+        actions: [
+          IconButton(onPressed: (){
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Click the Continue button')));
+          }, icon: Icon(Icons.arrow_forward_ios_rounded)),
+        ],
+      ),
         body: SafeArea(
             child: Center(
                 child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: [Text("Hi!")],
+      children: [
+        Image(image: AssetImage('assets/password_meme.jpg'),
+        height: MediaQuery.of(context).size.height*0.7,
+        width: MediaQuery.of(context).size.width*0.8),
+        SizedBox(height: MediaQuery.of(context).size.height*0.05,),
+        ElevatedButton(onPressed: (){}, child: const Text('Open'),),
+      ],
     ))));
   }
 }
